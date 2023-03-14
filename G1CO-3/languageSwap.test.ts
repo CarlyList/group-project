10e8c8529e24d1c53025a22335018b2e2f59e924
CarlyList <carlyliston8@gmail.com>
2023-03-13 19:10:35 -0500

    updated

---
+++ G1CO-3/languageSwap.test.ts
@@ -0,0 +1,12 @@
+import {Google} from './hondaPB'
+import {Builder, By, Capabilities, until, WebDriver, WebElement} from 'selenium-webdriver'
+import { Driver } from 'selenium-webdriver/chrome'
+const fs = require('fs')
+const honda = new Google()
+
+class
+
+test('Functionality of language swap', async () => {
+    await honda.navigate()
+    await driver.findElement(By.name)
+})
