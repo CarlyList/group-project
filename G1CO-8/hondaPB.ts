599674bc714ac34b0f536bab2a228d76957c0366
Alronil Pacheco <alronilpacheco10@gmail.com>
2023-03-15 17:42:32 -0700

    G1C0-8

--- G1CO-8/hondaPB.ts
+++ G1CO-8/hondaPB.ts
@@ -7,6 +7,9 @@ export class Google extends BasePage {
     submit: By = By.xpath('//button[@class="m_cta is-on-light is-secondary "]')
     owner: By = By.xpath('(//i[@class="mi-chevron-down"])[3]')
     mygarage: By = By.xpath('(//i[@class="mi-external"])[4]')
+    findadealer: By = By.xpath('//a[@href="/tools/dealership-locator"]')
+    search: By = By.xpath('//input[@name="placeLocation"]')
+    searchbutton: By = By.xpath('(//i[@class="dl-icon-search"])[1]')
     constructor() {
         super({url: "https://automobiles.honda.com/"});
     }
